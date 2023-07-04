dcb43722ced6330652dbd2f331f31d0ee7b59335
galu7777 <luisgalea31@gmail.com>
2023-07-04 13:13:15 -0400

    Creacion de Modelos y relaciones

---
+++ server/src/models/Product.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Product', {
+        id: {
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        higth: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        width: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        weigth: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        sku: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+};
