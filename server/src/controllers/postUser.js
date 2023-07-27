11e29cb82fb212bf4f6adfa4b725fe7121d4a1e8
galu7777 <luisgalea31@gmail.com>
2023-07-27 10:55:39 -0400

    super usuario

--- server/src/controllers/postUser.js
+++ server/src/controllers/postUser.js
@@ -2,7 +2,7 @@ const { User } = require("../db");
 const transporter = require("../middleware/nodemailer");
 
 const postUser = async (req, res) => {
-  const { full_name, email, password, phone, direction_shipping, displayName } = req.body;
+  const { full_name, email, password, phone, direction_shipping, displayName, role } = req.body;
   console.log("entro", full_name, email, password, phone, direction_shipping, displayName);
 
   try {
@@ -14,13 +14,45 @@ const postUser = async (req, res) => {
     if (existingUser) {
       return res.status(409).json({ status: 409, error: "Ya existe un usuario con ese email" });
     }
+    if(role){
+      const newUser = {
+        full_name: full_name || displayName,
+        email,
+        password,
+        phone,
+        direction_shipping,
+        role
+      };
+
+      await transporter.sendMail({
+        from: '"Onearket"<pf@gmail.com>', 
+        to: email, 
+        subject: "¡Bienvenido a ONE!",
+        html: `
+          <h1>Hola ${full_name},</h1>
+          <p>¡Bienvenido a ONE!</p>
+          <p>Estamos emocionados de tenerte como parte de nuestra comunidad.</p>
+          <p>En ONE, podrás disfrutar de una experiencia única de compras en línea con una amplia selección de productos y ofertas especiales.</p>
+          <p>No dudes en explorar nuestro catálogo y encontrar productos increíbles a precios inigualables.</p>
+          <p>Si tienes alguna pregunta o necesitas ayuda, nuestro equipo de soporte estará encantado de asistirte.</p>
+          <p>¡Gracias por unirte a nosotros!</p>
+          <p>Atentamente,</p>
+          <p>El equipo de ONE</p>
+        `});
+        
+        
+      const createUser = await User.create(newUser);
+      return res.status(200).json(createUser);
+
+    } else {
     const newUser = {
       full_name: full_name || displayName,
       email,
       password,
       phone,
-      direction_shipping,
+      direction_shipping,      
     };
+
     await transporter.sendMail({
       from: '"Onearket"<pf@gmail.com>', 
       to: email, 
@@ -40,6 +72,9 @@ const postUser = async (req, res) => {
       
     const createUser = await User.create(newUser);
     return res.status(200).json(createUser);
+  
+  }
+    
     
   } catch (error) {
     return res.status(500).json({ error: error });
