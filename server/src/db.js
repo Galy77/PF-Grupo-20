b015038bca23b27519c36f933b43f694203555c5
galu7777 <luisgalea31@gmail.com>
2023-07-27 00:33:01 -0400

    colocando db Internal

--- server/src/db.js
+++ server/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 
-const sequelize = new Sequelize('postgres://postgres:admin@localhost/ecommerce', {
+const sequelize = new Sequelize('postgres://pf:JoZSCTFoPpDDxTwuc6amy663hZq1GUt9@dpg-cil2di95rnuvtgto0omg-a/ecommerce_uhbm', {
   logging: false, 
   native: false, 
 });
