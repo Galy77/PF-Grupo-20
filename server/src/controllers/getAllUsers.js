c86d03ed2efb1d54f0be7221465f3e075fcf541c
galu7777 <luisgalea31@gmail.com>
2023-07-28 21:49:48 -0400

    agregando super user por default

--- server/src/controllers/getAllUsers.js
+++ server/src/controllers/getAllUsers.js
@@ -1,7 +1,21 @@
 const { User } = require("../db");
+
+const superUser = {
+  full_name: "super",
+  email: "superadmin@gmail.com",
+  password: "12345",
+  phone: 78945,
+  direction_shipping: "colinas 223",
+   role: 2
+}
+
 const getAllUsers = async (req, res) => {
     try {
       const users = await User.findAll();
+      if(users.length === 0){
+        const userCreated = await User.create(superUser)
+        return res.status(200).json(userCreated)
+      }
       return res.status(200).json(users);
     } catch (error) {
       return res.status(500).send({ error: error.message });
@@ -10,9 +24,13 @@ const getAllUsers = async (req, res) => {
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.findAll();    
-    const filterUsers = users.filter(us => us.status === 1 )
-    return res.status(200).json(filterUsers);
+    const users = await User.findAll();
+     if(users.length === 0){
+       const userCreated = await User.create(superUser)
+       return res.status(200).json(userCreated)
+     } 
+     const filterUsers = users.filter(us => us.status === 1 )
+     return res.status(200).json(filterUsers);
   } catch (error) {
     return res.status(500).send({ error: error.message });
   }
